33d2b901bf8894777105c31b7be773461533b44c
Ernesto <ernesto.solares.guerrero@gmail.com>
2019-08-10 01:14:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/public'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  // });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
