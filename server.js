73ce52c2242a5645a3ab1c0f0663d19adfa9996e
Ernesto <ernesto.solares.guerrero@gmail.com>
2019-08-09 20:41:13 -0500

    added send index.html to server

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const users = require("./routes/api/users");
 const patients = require("./routes/api/patients");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
