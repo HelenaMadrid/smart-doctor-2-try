6ea59bcf639f4adac125ac0cdd850fbea230d013
Ernesto <ernesto.solares.guerrero@gmail.com>
2019-08-09 20:45:15 -0500

    changed to public instead of build

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const patients = require("./routes/api/patients");
 const app = express();
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 // Bodyparser middleware
