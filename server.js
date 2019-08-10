d83d43c3aa800f012c64c62c1aea2a76369228e1
Ernesto <ernesto.solares.guerrero@gmail.com>
2019-08-10 01:49:05 -0500

    added run build

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const patients = require("./routes/api/patients");
 const app = express();
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/public'));
+  app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 // Bodyparser middleware
